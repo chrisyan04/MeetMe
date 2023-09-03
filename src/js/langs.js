1b7698f4d3fb6af52a5ee509fe9f29967c2b00f0
the creator <chenxi.yan@gmail.com>
2023-09-03 17:41:47 -0400

    Updating about page

--- src/js/langs.js
+++ src/js/langs.js
@@ -1,5 +1,6 @@
-function slideAndDisappear(entries, observer) {
-    entries.forEach((entry) => {
+// slide in from top
+function slideAndDisappear(entries1, observer1) {
+    entries1.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.style.transform = 'translateY(0)';
             entry.target.style.opacity = '1';
@@ -10,14 +11,40 @@ function slideAndDisappear(entries, observer) {
     });
 }
 
-const observer = new IntersectionObserver(slideAndDisappear, {
+const observer1 = new IntersectionObserver(slideAndDisappear, {
     root: null,
     rootMargin: '0px',
     threshold: 0.4,
 });
 
-const slideInElements = document.querySelectorAll('.slide-in');
+const slideInElements1 = document.querySelectorAll('.slide-in');
 
-slideInElements.forEach((element) => {
-    observer.observe(element);
+slideInElements1.forEach((element) => {
+    observer1.observe(element);
+});
+
+
+// slide in from bottom
+function slideInFromBottom(entries2, observer2) {
+    entries2.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.style.transform = 'translateY(0)';
+            entry.target.style.opacity = '1';
+        } else {
+            entry.target.style.transform = 'translateY(100%)';
+            entry.target.style.opacity = '0';
+        }
+    });
+}
+
+const observer2 = new IntersectionObserver(slideInFromBottom, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.4,
+});
+
+const slideInElements2 = document.querySelectorAll('.slide-in-from-bottom');
+
+slideInElements2.forEach((element) => {
+    observer2.observe(element);
 });
